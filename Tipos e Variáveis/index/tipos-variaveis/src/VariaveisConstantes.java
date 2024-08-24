6f6890452f3200604678d3978a4a886b71ea030b
DouglasReis07 <163474683+DouglasReis07@users.noreply.github.com>
2024-08-24 14:32:27 -0300

    variaveis

---
+++ Tipos e Variáveis/index/tipos-variaveis/src/VariaveisConstantes.java
@@ -0,0 +1,12 @@
+public class VariaveisConstantes {
+
+    public static void main(String[] args) {
+        final int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+    }
+    
+}
