fb05b3acbeb8ed2e292609a977c778a44006f1b6
DouglasReis07 <163474683+DouglasReis07@users.noreply.github.com>
2024-09-03 11:08:44 -0300

    tratamento java

--- Estruturas de repetiçoes/for/while/do while/do-while/src/App.java
+++ Estruturas de repetiçoes/for/while/do while/do-while/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int (x=1;x<=10;x++){ 
+            if(x % 2==0)
+            System.out.println(x); } 
     }
 }
